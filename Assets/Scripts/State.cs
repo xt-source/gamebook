58c8a5530b581a0be8b2d7a44a85f0a24b687287
John Doe <john.doe@example.com>
2020-10-12 00:43:03 +0200

    Add transition asset object

--- Assets/Scripts/State.cs
+++ Assets/Scripts/State.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Gamebook
 {
     [CreateAssetMenu]
     public class State : ScriptableObject
     {
+        [SerializeField]
+        private List<Transition> m_Transitions = new List<Transition>();
 
+
+        public List<Transition> transitions { get => m_Transitions; set => m_Transitions = value; }
     }
 }
---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Gamebook
+{
+    [CreateAssetMenu]
+    public class Transition : ScriptableObject
+    {
+        [SerializeField]
+        private State m_Source;
+
+
+        [SerializeField]
+        private State m_Target;
+
+
+        public State source { get => m_Source; set => m_Source = value; }
+
+
+        public State target { get => m_Target; set => m_Target = value; }
+    }
+}
