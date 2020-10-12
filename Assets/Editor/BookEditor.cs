7ac04d0f893344884900f92a2b07d8f86fba6c38
John Doe <john.doe@example.com>
2020-10-12 10:26:43 +0200

    Add book asset association

--- Assets/Editor/BookEditor.cs
+++ Assets/Editor/BookEditor.cs
@@ -1,14 +1,59 @@
 using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace Gamebook
 {
     public class BookEditor : EditorWindow
     {
+        public Book book { get; private set; }
+
+
         [MenuItem("Window/Book Editor")]
         private static BookEditor GetEditor()
         {
             BookEditor editor = GetWindow<BookEditor>("Book Editor", true, typeof(SceneView));
             return editor;
+        }
+
+
+        [OnOpenAsset]
+        private static bool OnOpenAsset(int instanceID, int line, int column)
+        {
+            string assetPath = AssetDatabase.GetAssetPath(instanceID);
+            Book book = AssetDatabase.LoadAssetAtPath<Book>(assetPath);
+            if (book == null)
+            {
+                return false;
+            }
+
+            BookEditor editor = GetEditor();
+            editor.Load(book);
+            return true;
+        }
+
+
+        private void OnSelectionChange()
+        {
+            Book book = Selection.activeObject as Book;
+            if (book == null)
+            {
+                return;
+            }
+
+            Load(book);
+        }
+
+
+        private void Load(Book book)
+        {
+            if (book == null)
+            {
+                return;
+            }
+
+            this.book = book;
+            Repaint();
         }
     }
 }
